d70f0962e57aa2e0533776833c9d137443659754
Lilseec <44337171+Lilseec@users.noreply.github.com>
2022-01-27 08:33:15 +0200

    full redisign

--- main.js
+++ main.js
@@ -1,82 +1,75 @@
-let btn = document.getElementById('add')
-let container = document.querySelector('.notes')
-let note = document.querySelector('.noteLink')
-let textArea = document.getElementById('input')
+const tx = document.getElementsByTagName("textarea");
+const btn = document.querySelector('.button')
+const allNotes = document.querySelector('.Notes')
+allNotes.addEventListener('click', (e) => {
 
+})
 
-
-for (let key in localStorage) {
-    if (key === 'key' || key ==='getItem' || key === 'clear' || key === 'setItem' || key === 'removeItem' || key === 'length') {
-        // pass
-    } else {
-        createNote(localStorage[key], key)
-    }
-    
+for (const [key, value] of Object.entries(localStorage)) {
+  createNote(key, value)
 }
 
 
-
-function createNote(noteText, header) {
-    let linkToNote = document.createElement('p')
-    linkToNote.id = header
-    linkToNote.className = 'noteLink'
-    
-    localStorage.setItem(header, noteText)
-    linkToNote.textContent = header
-    container.appendChild(linkToNote)
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].addEventListener("input", OnInput, false);
 }
 
-function createNoteWindow(nodeText, header) {
-    let div = document.createElement("div");
-    div.id = 'note'
-    let head = document.createElement('span')
-    head.id = 'header'
-    head.textContent = header
-    let close = document.createElement('span')
-    close.id = 'close'
-    close.textContent = 'X'
-    let text = document.createElement('p')
-    text.textContent = nodeText
-    text.innerHTML = text.innerHTML.replace(/\n/g, '<br>\n');  
-    close.addEventListener('click', () => {
-        div.style.opacity = '0'
-        close.style.opacity = '0'
-        setTimeout(() => div.remove(), 250)
-        setTimeout(() => close.remove(), 250)
-    })
-    div.appendChild(head)
-    div.appendChild(close)
-    div.appendChild(text)
-    document.body.appendChild(div)
-    div.animate([
-        // keyframes
-        { opacity: '0' },
-        { opacity: '1' }
-    ], {
-        // timing options
-        duration: 250,
-    });
+function OnInput() {
+  this.style.height = "auto";
+  this.style.height = (this.scrollHeight) + "px";
 }
+
+const btns = document.querySelectorAll(".main-header span");
+const container = document.querySelector('.overflow')
+for (const btn of btns) {
+    btn.addEventListener('click', (e) => {
+        let current = document.getElementById('current')
+        let next = document.querySelector('.' + e.target.textContent)
+        let clientHeight = document.querySelector('.container').clientHeight
+        container.style = 'height:' + clientHeight.toString() + 'px;' + 'opacity: 1'
+        setTimeout(() => {
+          container.style = 'height:' + clientHeight.toString() + 'px;' + 'opacity: 0'
+          current.style.display = 'none'
+          current.id = ''
+          next.style.display = '';
+          next.id = 'current'
+          
+          setTimeout(() => container.style = '', 150)
+          
+        }, 300)
+      })
+}
+
 
 btn.addEventListener('click', () => {
-    let textToParse = textArea.value;
-    let symbolIdx = 0
-    while (true) {
-        symbolIdx = textToParse.indexOf("\n")
-        if (symbolIdx === -1) {
-            symbolIdx = textToParse.length
-            break
-        }
-        break
+  let textToParse = tx[0].value;
+  let symbolIdx = 0
+  while (true) {
+    symbolIdx = textToParse.indexOf("\n")
+    if (symbolIdx === -1) {
+        symbolIdx = textToParse.length
+      }
+     break
     }
     let header = textToParse.substring(0, symbolIdx)
-    textToParse.replace(/\n/g, '<br>\n');
-    console.log(textToParse)
-    createNote(textToParse, header)
-
+    createNote(header, textToParse.replace(/\n/g, '<br>\n'))
 })
 
-container.addEventListener('click', (event) => {
-    let note = localStorage.getItem(event.target.id)
-    createNoteWindow(note, event.target.id)
-})
+function createNote(headerText, noteText) {
+  
+  localStorage.setItem(headerText, noteText)
+  let note = document.createElement('div')
+  note.className = 'note'
+  let noteHeader = document.createElement('div')
+  noteHeader.className = 'note-header'
+  let header = document.createElement('h1')
+  header.textContent = headerText
+  noteHeader.appendChild(header)
+  let noteContent = document.createElement('div')
+  noteContent.className = 'note-content'
+  noteContent.innerHTML = noteText
+  note.appendChild(noteHeader)
+  note.appendChild(noteContent)
+  allNotes.appendChild(note)
+}
